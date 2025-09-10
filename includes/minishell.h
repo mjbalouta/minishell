75aabe821c648a3f6d4edd2ff6a7714c8f0c10da
josemigu <josemigu@student.42porto.com>
2025-09-10 18:30:09 +0100

    feat: implementing main loop

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,8 +1,31 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# define PROMPT "minishell$ "
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
 # include "./libft-projects/get-next-line/get_next_line.h"
 # include "./libft-projects/libft/libft.h"
 # include "./libft-projects/printf/libftprintf.h"
-teste
+
+enum e_token_type
+{
+	T_WORD,
+	T_REDIRECT_INPUT,
+	T_REDIRECT_OUTPUT,
+	T_HEREDOC,
+	T_REDIRECT_OUTPUT_APPEND,
+	T_PIPE
+};
+
+typedef struct s_token
+{
+	char			*word;
+	e_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,13 @@
-int	main(void)
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
 {
-	
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
