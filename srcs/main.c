2923c2f540e1da839e3a80ec496acd9aa098523e
Maria Joao Fraga Balouta <mjoao-fr@c2r1s2.42porto.com>
2025-09-15 17:51:18 +0100

    add: linked lists in main for tokens and commands

--- srcs/main.c
+++ srcs/main.c
@@ -5,6 +5,8 @@ int	g_signal_number;
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell	ms;
+	t_token *tokens;
+	t_command *commands;
 
 	(void)argv;
 	(void)envp;
@@ -20,6 +22,7 @@ int	main(int argc, char **argv, char **envp)
         }
 		if (*ms.input)
 			add_history(ms.input);
+		commands = create_cmd_list(tokens);
 		free(ms.input);
 	}
 	return (ms.exit_status);
