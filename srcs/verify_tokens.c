f42169b303b072f984b2ba9d63e41d316ed3304f
Maria Joao Fraga Balouta <mjoao-fr@c2r2s8.42porto.com>
2025-09-17 14:15:27 +0100

    added: verify_tokens.c complete (only missing testing)

--- srcs/verify_tokens.c
+++ srcs/verify_tokens.c
@@ -1,34 +1,50 @@
 #include "../includes/minishell.h"
 
 /**
- * @brief verifies if there are empty commands or if filenames are missing
+ * @brief verifies if there are empty commands, no commands or if filenames are missing
  * 
  * @param ms 
  * @return -1 for error or 0 if everything is correct
  */
 int	verify_tokens(t_shell *ms)
 {
-	int	first_token;
+	int		first_token;
+	t_token	*temp;
+	int		is_command;
 
 	first_token = 0;
-	while (ms->token)
+	temp = ms->token;
+	while (temp)
 	{
-		// if ()
-		if (is_redir(ms->token) == 1)
+		if (first_token == 0 && temp->type == T_PIPE) // se o primeiro token for um pipe
+			return (-1);
+		is_command = 0;
+		while (temp && temp->type != T_PIPE)
 		{
-			if (!ms->token->next) //se nao existir token depois de um redirect
+			if (temp->type == T_WORD)
+				is_command = 1;
+			first_token = 1;
+			if (is_redir(temp) == 1)
+			{
+				if (!temp->next) //se nao existir token depois de um redirect
+					return (-1);
+				if (temp->next->type != T_WORD) //se o token a seguir ao redirect nao for uma palavra
+					return (-1);
+			}
+			temp = temp->next;
+		}
+		if (is_command == 0) // o primeiro "comando" nao contem uma palavra/um comando
+			return (-1);
+		if (!temp)
+			break ;
+		if (temp->type == T_PIPE)
+		{
+			if (!temp->next) // se nao existir token a seguir a um pipe
 				return (-1);
-			if (ms->token->next->type != T_WORD) //se o token a seguir ao redirect nao for uma palavra
+			if (temp->next->type == T_PIPE) // se o token a seguir ao pipe for outro pipe
 				return (-1);
 		}
-		if (ms->token->type == T_PIPE)
-		{
-			if (!ms->token->next) // se nao existir token a seguir a um pipe
-				return (-1);
-			if (ms->token->next->type == T_PIPE) // se o token a seguir ao pipe for outro pipe
-				return (-1);
-		}
-		ms->token = ms->token->next;
+		temp = temp->next;
 	}
 	return (0);
 }
