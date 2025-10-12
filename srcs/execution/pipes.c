392707cbfceb35622252410d496a2067f790498f
mjoao-fr <mjbalouta@gmail.com>
2025-10-12 19:19:02 +0100

    fix: leaks

--- srcs/execution/pipes.c
+++ srcs/execution/pipes.c
@@ -13,10 +13,9 @@ void	execute_pipe_cmd(int *pipefd, t_shell *ms, int prev_fd, t_command *command)
 	char		**envp;
 
 	handle_redir(ms, pipefd, prev_fd, command);
-	if (prev_fd != -1)
-		close (prev_fd);
+	close_one_fd(prev_fd);
 	if (ms->i < ms->nr_commands - 1)
-		close(pipefd[1]);
+		close_both_fds(pipefd);
 	if (!command->args[0])
 		exit_shell(ms, 0);
 	verify_comm_path(command, ms);
@@ -34,9 +33,15 @@ void	execute_pipe_cmd(int *pipefd, t_shell *ms, int prev_fd, t_command *command)
 			free_char_array(envp);
 			exit_shell(ms, 1);
 		}
-		else
-			free_char_array(envp);
 	}
+}
+
+void	execute_single_cmd_builtin(t_command *cmd, t_shell *ms, int prev_fd, int *pipefd)
+{
+	if (cmd->redir && cmd->redir->type == T_HEREDOC)
+			handle_heredoc_input(cmd, ms);
+	handle_redir(ms, pipefd, prev_fd, cmd);
+	execute_builtin(ms, cmd);
 }
 
 /**
@@ -56,12 +61,7 @@ void	handle_processes(t_shell *ms)
 	prev_fd = -1;
 	temp = ms->command;
 	if (ms->nr_commands == 1 && temp->is_builtin == 0)
-	{
-		if (temp->redir && temp->redir->type == T_HEREDOC)
-			handle_heredoc_input(temp, ms);
-		handle_redir(ms, pipefd, prev_fd, temp);
-		execute_builtin(ms, temp);
-	}
+		execute_single_cmd_builtin(temp, ms, prev_fd, pipefd);
 	else
 	{
 		while (++ms->i < ms->nr_commands && temp)
@@ -70,24 +70,20 @@ void	handle_processes(t_shell *ms)
 				create_pipe(pipefd, ms);
 			if (temp->redir && temp->redir->type == T_HEREDOC)
 				handle_heredoc_input(temp, ms);
-			// if (temp->args[0])
-			// {
 			ms->pid[id] = fork();
 			if (ms->pid[id] < 0)
 				exit_shell(ms, 1);
 			if (ms->pid[id++] == 0)
 				execute_pipe_cmd(pipefd, ms, prev_fd, temp);
-			// }
+			close_one_fd(prev_fd);
 			if (ms->i < ms->nr_commands - 1)
 			{
-				if (pipefd[1] != -1)
-					close(pipefd[1]);
+				close_one_fd(pipefd[1]);
 				prev_fd = pipefd[0];
 			}
 			temp = temp->next;
 		}
-		if (prev_fd != -1)
-			close(prev_fd);
+		close_one_fd(prev_fd);
 		g_exit_status = wait_for_child(ms, id);
 	}
 }
